0fa0938009b29f1cd988918711c67b71d0fee094
Kingstimusprime <kingston.james7710@gmail.com>
2023-10-10 17:59:16 -0400

    optimized

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
 const WebSocket = require('ws');
-const ws = new WebSocket.Server({ port: 7071 }); //init a websockets
+const ws = new WebSocket({
+    port: 8080,
+    perMessageDeflate: {
+      zlibDeflateOptions: {
+        // See zlib defaults.
+        chunkSize: 1024,
+        memLevel: 7,
+        level: 3
+      },
+      zlibInflateOptions: {
+        chunkSize: 10 * 1024
+      },
+      // Other options settable:
+      clientNoContextTakeover: true, // Defaults to negotiated value.
+      serverNoContextTakeover: true, // Defaults to negotiated value.
+      serverMaxWindowBits: 10, // Defaults to negotiated value.
+      // Below options specified as default values.
+      concurrencyLimit: 10, // Limits zlib concurrency for perf.
+      threshold: 1024 // Size (in bytes) below which messages
+      // should not be compressed if context takeover is disabled.
+    }
+  });
 
 const clients = new Map(); // everyone connected
 const servers = new Map();
